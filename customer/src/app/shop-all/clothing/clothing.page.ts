d909fb5974a9ebff2ccc5e751584ea6733edda73
u20570750 <103131462+u20570750@users.noreply.github.com>
2023-07-31 00:23:47 +0200

    Customer

--- customer/src/app/shop-all/clothing/clothing.page.ts
+++ customer/src/app/shop-all/clothing/clothing.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule, ModalController } from '@ionic/angular';
+import { IonicModule, ModalController,AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { Stock_Item } from 'src/app/Models/stockitem';
 
 @Component({
   selector: 'app-clothing',
@@ -12,8 +13,10 @@ import { Router } from '@angular/router';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class ClothingPage implements OnInit {
+  menuType: string = 'overlay';
 
-  constructor(private _modalController:ModalController,private _router:Router) { }
+  stockItem: Stock_Item[] = [];
+  constructor(private _modalController: ModalController, private _router: Router, private alertController:AlertController) { }
 
   ngOnInit() {
   }
@@ -28,5 +31,75 @@ export class ClothingPage implements OnInit {
   }
   public Basket() {
     this._router.navigate(["/tabs/basket"])
+  }
+
+  //Load Stock Items
+  updateGrid() {
+    var grid = document.querySelector("#grid");
+
+    if (grid) {
+      grid.innerHTML = " ";
+      for (var i = 0; i < this.stockItem.length; i++) {
+        `<ion-row>` +
+          `<ion-col>` +
+          `<ion-card>` +
+          `<ion-card-title>Name: ${this.stockItem[i].Stock_Item_Name}</ion-card-title>` +
+          `<ion-card-content>` +
+          `<ion-img>${this.stockItem[i].stockimage.Stock_Image_File}</ion-img>` +
+          `<p>${this.stockItem[i].stockitemcolours.Stock_Item_Colour_Name}</p>` +
+          `<p>R: ${this.stockItem[i].Stock_Item_Price}</p>` +
+          `</ion-card-content>` +
+          `</ion-card>` +
+          `<ion-button (click)="addToBasket(${this.stockItem[i].Stock_Item_ID})" >Add To Basket</ion-button>` +
+          `</ion-col>` +
+          `<ion-row>`;
+      }
+
+      //visibility();
+      //Subtotal();
+    }
+    //save to local storage
+    // localStorage.setItem("cart",JSON.stringify(cart));
+  }
+
+  /*!!!!!!!!!!!!!!UPDATE!!!!!!!!!!! */
+  addToBasket(){
+//const item=this.stockItem.find((stockItem)=>stockItem.Stock_Item_ID==id);
+this.addToBasketSuccessAlert();
+//alert("Added to basket successfully")
+  }
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+  reloadPage(){
+    window.location.reload()
+  }
+
+  async addToBasketSuccessAlert() {
+    const alert = await this.alertController.create({
+      header: 'Success!',
+      subHeader: 'Added to basket.',
+      buttons: [{
+          text: 'OK',
+          role: 'cancel',
+          handler:() =>{
+            this.reloadPage();
+          }
+      }],
+    });
+    await alert.present();
+  }
+  async addToBasketErrorAlert() {
+    const alert = await this.alertController.create({
+      header: 'We are sorry!',
+      subHeader: 'Discount Was Not Added',
+      message: 'Please try again',
+      buttons: [{
+        text: 'OK',
+        role: 'cancel',
+        handler:() =>{
+          this.reloadPage();
+        }
+    }],
+    });
+    await alert.present();
   }
 }
