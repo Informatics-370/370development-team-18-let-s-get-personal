e9f2ffc8808cf07ac671f8ee7f3dff0f307357b4
u20570750 <103131462+u20570750@users.noreply.github.com>
2023-09-20 11:56:20 +0200

    help-search

--- customer/src/app/help/help.page.ts
+++ customer/src/app/help/help.page.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ActivatedRoute, Router, RouterModule, NavigationEnd } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -10,15 +11,19 @@ import { ActivatedRoute, Router, RouterModule, NavigationEnd } from '@angular/ro
   templateUrl: './help.page.html',
   styleUrls: ['./help.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule,RouterModule]
+  imports: [IonicModule, CommonModule, FormsModule, RouterModule]
 })
 export class HelpPage implements OnInit {
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient) { }
+
+  searchQuery: string = '';
+  documentContent: string = '';
+  highlightedWord:string='';
 
   ngOnInit() {
     // Subscribe to route fragment changes
-    this.route.fragment.subscribe((fragment: string|null) => {
+    this.route.fragment.subscribe((fragment: string | null) => {
       // Check if there's a fragment and scroll to it
       if (fragment) {
         const element = document.getElementById(fragment);
@@ -27,6 +32,46 @@ export class HelpPage implements OnInit {
         }
       }
     });
+
+    /*this.http.get('help/help.page.html').subscribe((data: any) => {
+      if (data && data.content) {
+        this.documentContent = data.content;
+      } else {
+        this.documentContent = 'Document not found.';
+      }
+  console.log(this.documentContent)
+    });*/
+
+  }
+  searchDocument() {
+    
+    // Clear previous highlighting
+    this.highlightedWord = '';
+
+    if (this.searchQuery) {
+      const documentContentElement = document.getElementById(this.searchQuery);
+      if (documentContentElement) {
+
+        const content = documentContentElement.innerHTML;
+        // Create a regular expression to search for the query case-insensitively
+        const regex = new RegExp(this.searchQuery, 'gi');
+
+         // Highlight the matches
+         const highlightedContent = content.replace(
+          regex,
+          (match) =>
+            `<span class="highlighted-text">${match}</span>`
+        );
+
+        documentContentElement.innerHTML = highlightedContent;
+        // Scroll to the first highlighted match
+        const firstHighlight = document.querySelector('.highlighted-text');
+        if (firstHighlight) {
+          firstHighlight.scrollIntoView();
+          this.highlightedWord = this.searchQuery;
+        }
+      }
+    } 
   }
 
   public faq() {
