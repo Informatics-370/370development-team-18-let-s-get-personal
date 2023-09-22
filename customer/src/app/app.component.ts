2bf2150446500adc5084f8e8a556f878c5eb3dda
u20570750 <103131462+u20570750@users.noreply.github.com>
2023-09-22 15:25:45 +0200

    Google-API - Delivery-Price

--- customer/src/app/app.component.ts
+++ customer/src/app/app.component.ts
@@ -3,15 +3,20 @@ import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, HttpClientModule],
+  imports: [IonicModule, CommonModule, HttpClientModule,FormsModule],
 })
 export class AppComponent {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {}
+
+  onPredictionSelected(prediction: string) {
+    console.log('Selected Prediction:', prediction);
+  }
 }
--- customer/src/app/basket/make-payment/make-payment.page.ts
+++ customer/src/app/basket/make-payment/make-payment.page.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit,Output,ViewChild,EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DeliveryAddress } from 'src/app/Models/deliveryaddress';
 import { DeliveryDataService } from 'src/app/Services/deliveries.service';
 import { Delivery_Company } from 'src/app/Models/deliverycompany';
 import { AlertController, IonicModule } from '@ionic/angular';
-import { RouterModule, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { OrderRequestService } from 'src/app/Services/orderrequest.service';
 import { DeliveryVM } from 'src/app/ViewModels/deliveryVM';
@@ -16,6 +16,8 @@ import { OrderT } from 'src/app/Models/basket';
 import { Delivery } from 'src/app/Models/delivery';
 import { AuditTrail } from 'src/app/Models/audittrail';
 import { AuditTrailService } from 'src/app/Services/audittrail.service';
+import { NavController } from '@ionic/angular';
+import { PlacesService } from 'src/app/Services/PlacesService ';
 
 @Component({
   selector: 'app-make-payment',
@@ -32,13 +34,23 @@ export class MakePaymentPage implements OnInit {
   addedaddres!: DeliveryAddress;
   addeddeliveryrequest!: Delivery;
 
+
+  oLong:number=28.2310411;
+  oLat:number=-25.756598;;
+  Long!:number;
+  Lat!:number;
+
+  chargeRatePerKm:any=10;
+
+  distanceInKm:any;
+
   ngOnInit() {
     this.getDeliveryCompany();
   }
 
   @ViewChild(IonModal) modal!: IonModal
   constructor(private service:OrderRequestService, private router: Router, public modalCtrl: ModalController,
-    private alertController:AlertController, public delservice: DeliveryDataService, private auditservice: AuditTrailService) { }
+    private alertController:AlertController, public delservice: DeliveryDataService, private auditservice: AuditTrailService,public navCtrl: NavController,private placesService: PlacesService) { }
 
 
     validProvinces = ['Limpopo', 'Gauteng', 'North West','Kwa-Zulu Natal','Eastern Cape','Mpumalanga','Western Cape','Free State','Northern Cape'];
@@ -100,14 +112,96 @@ export class MakePaymentPage implements OnInit {
       console.error('add address error:', error);
     });
   }
+
+  @Output() predictionSelected = new EventEmitter<string>();
+
+  searchControl = new FormControl();
+  showPredictions = false;
+  predictions: any[] = [];
+
+updateSearchResults() {
+    const query = this.searchControl.value;
+    if (query && query.length >= 3) {
+      this.placesService.getPlacePredictions(query).subscribe(
+        (predictions) => {
+          this.predictions = predictions;
+          this.showPredictions = true;
+        },
+        () => {
+          this.predictions = [];
+          this.showPredictions = false;
+        }
+      );
+    } else {
+      this.predictions = [];
+      this.showPredictions = false;
+    }
+  }
+
+  selectPrediction(prediction: any) {
+    this.searchControl.setValue(prediction.description);
+    this.showPredictions = false;
+    this.predictionSelected.emit(prediction.description);
+    console.log('Location:',prediction.description);
+    this.getCordinate(prediction.description)
+
+    console.log("Distacnce:" +this.distanceInKm)
+
+     //this.calculateDistance(this.oLat,this.oLong,this.Lat,this.Long);
+  }
+  deliveryPrice:any;
+  async getCordinate(address:any){
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=AIzaSyDmbh7Bh6P-aR-w9LJMdy6c3zTVhDd-K3A`)
+    .then(response=>response.json()).then(data=>{
+
+      console.log(data)
+
+      if(data.status==="OK" && data.results.length>0){
+        const location=data.results[0].geometry.location;
+
+        this.Lat= location.lat;
+        this.Long=location.lng;    
+        this.distanceInKm=this.calculateDistance(this.oLat,this.oLong,this.Lat,this.Long);
+        console.log(this.distanceInKm)
+        const charge = (this.distanceInKm *this.chargeRatePerKm).toFixed(2); // Round to the nearest 100
+        this.deliveryPrice=charge;
+        console.log("Price based on distance "+charge)
     
- 
+    }else{
+      console.error("Something is wrong");
+    }
+        
+    }).catch(err=>console.error("Error"+err));
+  }
+    
+  calculateDistance(oLat:number,oLong:number,lat:number,long:number):number{
+    const radOLat = (Math.PI / 180) * oLat;
+    const radOLong = (Math.PI / 180) * oLong;
+    const radLat = (Math.PI / 180) * lat;
+    const radLong = (Math.PI / 180) * long;
+
+    const dLat = radLat - radOLat;
+    const dLon = radLong - radOLong;
+
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(radOLat) * Math.cos(radLat) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const distance = 6371 * c; // Radius of the Earth in kilometers
+
+    return distance;
+
+  }
+  
+  
+
 
   AddDeliveryRequest(){
     let addressID = JSON.parse(JSON.stringify(localStorage.getItem('addressID')));
       let addDeliveryRequest = new Delivery();
       addDeliveryRequest.delivery_Address_ID = this.addedaddres.delivery_Address_ID //this.addedaddres.delivery_Address_ID addressID
       addDeliveryRequest.delivery_Company_ID = this.AddDelAddressForm.value.deliveryCompanyID
+
+      console.log(this.deliveryPrice)
 
       this.service.AddDeliveryRequest(addDeliveryRequest).subscribe(res =>{
         let added = res as Delivery;
